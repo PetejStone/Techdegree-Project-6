e119caf6d56977fcd9e850873dea037165634a15
PetejStone <petejstoney@gmail.com>
2017-12-07 15:36:09 -0800

    added more JS
    added more JS

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 
+let subtitles = document.getElementsByClassName("mejs__time-float-current");
+let pCurrent = document.querySelectorAll('p');
+let video = document.getElementById('video');
+
+
+
+// video.addEventListener('ontimeupdate', () => {
+// //   if (video.currentTime = 2) {
+// //     pCurrent.style.color = 'red';
+// //   }
+// // });
+//
+// if (video.currentTime = 2000) {
+//   pCurrent.style.color = 'red';
+//   el
+// }
